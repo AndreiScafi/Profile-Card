46b23307fe53721d611bae8d94e4d3182bc23298
Andrei Scafi <andreiscafi@hotmail.com>
2023-10-23 08:48:11 -0300

    Making the button work with Javascript.

---
+++ script.js
@@ -0,0 +1,6 @@
+const card = document.querySelector('.card');
+const cardBtn = document.querySelector('.card-btn');
+
+cardBtn.addEventListener('click', () => {
+    card.classList.toggle('change');
+})
